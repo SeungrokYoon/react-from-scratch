7afa567186b43faa77e5cd1c012b4b97735dfb99
Seungrok <sonicrok93@gmail.com>
2023-07-25 17:39:06 +0900

    feat: add propTypes to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,18 @@
-export default function Button() {
-  return <button type="button">button</button>;
+import PropTypes from 'prop-types';
+
+export default function Button({ children, className = '' }) {
+  return (
+    <button type="button" className={className}>
+      {children}
+    </button>
+  );
 }
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  children: PropTypes.element.isRequired,
+  className: PropTypes.string,
+};
