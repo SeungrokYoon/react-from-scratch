d2a22a995f1cca3fb5d0b304bb2fb7e67d15abda
Seungrok <sonicrok93@gmail.com>
2023-07-26 23:10:20 +0900

    feat: remove default margin and padding

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './global.css';
 import App from './App';
 
 const domNode = document.getElementById('root');
