2d270c95567e6c4005f9e1c6538502a46efaaf16
SeungrokYoon <sonicrok93@gmail.com>
2024-03-05 23:37:31 +0900

    feat: input 표준 스펙 속성 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -7,55 +7,59 @@ export default function SignUp() {
   const { formData, setFormData } = useForm();
   return (
     <PageLayout>
-      <form>
-        <div>회원가입</div>
-        <label>
-          email
-          <input
-            type="email"
-            value={formData.email}
-            onChange={(e) => {
-              setFormData.setEmail(e.target.value);
+      <div>
+        <form>
+          <div>회원가입</div>
+          <label>
+            email
+            <input
+              type="email"
+              value={formData.email}
+              onChange={(e) => {
+                setFormData.setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            password
+            <input
+              type="password"
+              value={formData.password}
+              onChange={(e) => {
+                setFormData.setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              const auth = getAuth();
+              createUserWithEmailAndPassword(
+                auth,
+                formData.email,
+                formData.password
+              )
+                .then((userCredential) => {
+                  // Signed in
+                  const user = userCredential.user;
+                  // ...
+                  alert('회원가입이 완료되었습니다');
+                })
+                .catch((error) => {
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  alert(errorCode + errorMessage);
+                });
             }}
-          />
-        </label>
-        <label>
-          password
-          <input
-            type="password"
-            value={formData.password}
-            onChange={(e) => {
-              setFormData.setPassword(e.target.value);
-            }}
-          />
-        </label>
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            const auth = getAuth();
-            createUserWithEmailAndPassword(
-              auth,
-              formData.email,
-              formData.password
-            )
-              .then((userCredential) => {
-                // Signed in
-                const user = userCredential.user;
-                // ...
-                alert('회원가입이 완료되었습니다');
-              })
-              .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                alert(errorCode + errorMessage);
-              });
-          }}
-        >
-          submit
-        </button>
-      </form>
-      <Form />
+          >
+            submit
+          </button>
+        </form>
+      </div>
+      <div>
+        <Form />
+      </div>
     </PageLayout>
   );
 }
