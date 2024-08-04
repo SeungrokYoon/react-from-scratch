e54d2053f6687d2ffb39da6e56c4f493ab9e7f03
SeungrokYoon <sonicrok93@gmail.com>
2024-08-04 21:22:04 +0900

    :recycle: Refactor :  (4) Errorboundary 사용으로, 렌더링 에러에 대한 fallback UI를 보여주기

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,43 @@
+import { Component, PropsWithChildren, ReactNode } from 'react';
+
+interface ErrorBoundaryProps extends PropsWithChildren {
+  fallback: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo): void {
+    // Example "componentStack":
+    //   in ComponentThatThrows (created by App)
+    //   in ErrorBoundary (created by App)
+    //   in div (created by App)
+    //   in App
+    console.error('Error caught by ErrorBoundary:', error);
+    console.error('Component stack:', info.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
--- src/components/User/UserManagement.tsx
+++ src/components/User/UserManagement.tsx
@@ -1,6 +1,7 @@
 import useUsers from '@Hook/useUsers';
 import UserForm from './UserForm';
 import UserList from './UserList';
+import ErrorBoundary from '@Component/ErrorBoundary';
 
 export default function UserManagement() {
   const { users, isLoading, error, addUser, deleteUser } = useUsers();
@@ -8,10 +9,12 @@ export default function UserManagement() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
   return (
-    <div>
-      <h1>User Management</h1>
-      <UserForm onSubmit={addUser} />
-      <UserList users={users} onDeleteUser={deleteUser} />
-    </div>
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <div>
+        <h1>User Management</h1>
+        <UserForm onSubmit={addUser} />
+        <UserList users={users} onDeleteUser={deleteUser} />
+      </div>
+    </ErrorBoundary>
   );
 }
