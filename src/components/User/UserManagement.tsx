a74bed8b4cb9f69ee09b7d0502a69a364578bf32
SeungrokYoon <sonicrok93@gmail.com>
2024-08-04 17:35:21 +0900

    :recycle: Refactor :  (3) UserList컴포넌트로 분리하여 선언적인 컴포넌트로 추상화하기

--- src/components/User/UserManagement.tsx
+++ src/components/User/UserManagement.tsx
@@ -1,5 +1,6 @@
 import useUsers from '@Hook/useUsers';
 import UserForm from './UserForm';
+import UserList from './UserList';
 
 export default function UserManagement() {
   const { users, isLoading, error, addUser, deleteUser } = useUsers();
@@ -10,16 +11,7 @@ export default function UserManagement() {
     <div>
       <h1>User Management</h1>
       <UserForm onSubmit={addUser} />
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <span>
-              {user.firstName}&nbsp;{user.lastName}
-            </span>
-            <button onClick={() => deleteUser(user.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <UserList users={users} onDeleteUser={deleteUser} />
     </div>
   );
 }
