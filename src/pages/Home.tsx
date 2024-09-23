2ac5444a6bc19ca3c4b92a553e7b79d37c1aeb09
SeungrokYoon <sonicrok93@gmail.com>
2024-09-23 12:43:54 +0900

    :sparkles: Feat :  add Sidebar page route to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,6 +73,9 @@ export default function Home() {
       <div>
         <Link to="/user">User</Link>
       </div>
+      <div>
+        <Link to="/sidebar">Sidebar</Link>
+      </div>
       <FlexCol>
         <label htmlFor="aNum">
           aNum
