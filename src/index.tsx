a280f94e49df801498c5344c01c3b140e4318907
SeungrokYoon <sonicrok93@gmail.com>
2026-01-31 20:19:23 +0900

    fix: Null type

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './global.css';
 import App from './App';
 
 const domNode = document.getElementById('root');
+if (!domNode) {
+  throw new Error('Failed to find root element');
+}
 const root = createRoot(domNode);
 root.render(
   <StrictMode>
